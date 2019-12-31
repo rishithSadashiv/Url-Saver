ebd60f71576d454926bdc96cd6df9d697da0214f
rishithSadashiv <rishith.1si16ec078@gmail.com>
2019-12-31 21:39:10 +0530

    signup button of Login screen actuated

---
+++ app/src/main/java/com/example/urlsaver/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.urlsaver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button signup = findViewById(R.id.buttonSignupLogin);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
